833bde436f7edaa0be11ef1594737228afe5929b
Constant Dupuis <constant.dupuis@gmail.com>
2021-10-06 12:50:42 +0200

    Draw out of the box

--- mySketchQT/src/PerlinNoiseFlowSketch.h
+++ mySketchQT/src/PerlinNoiseFlowSketch.h
@@ -14,8 +14,10 @@ public:
     ///
 	void setup() {
         gui_.setup();
+        gui_.add(fullScreen_.setup("full screen", false));
         gui_.add(animate_.setup("animate", false));
         gui_.add(animationSpeed_.setup("aimation speed", 0.5, 0.01, 1.0));
+        gui_.add( quit_.setup("exit"));
         setupResize();
 	}
 	
@@ -34,7 +36,7 @@ public:
     /// \brief draw
     ///
 	void draw() {
-        fbo_->draw((ofGetWidth()-size_) * .5, (ofGetHeight()-size_)*.5f);
+        fbo_->draw(0, 0);
         gui_.draw();
 	}
 
@@ -67,10 +69,13 @@ private:
     float animation_ = 0.0f;
     //float animationSpeed_= 0.5f;
     ofColor background_ = ofColor(60);
+    glm::vec2 topLeft_;
 
     ofxPanel gui_;
     ofxToggle animate_;
     ofxFloatSlider animationSpeed_;
+    ofxToggle fullScreen_;
+    ofxButton quit_;
 
     ///
     /// \brief setupResize
@@ -78,13 +83,16 @@ private:
     void setupResize()
     {
         valueGrid_.reset();
-        size_ = ofGetHeight() * 0.8f;
+        auto ww = ofGetWidth();
+        auto wh = ofGetHeight();
+        size_ = ofGetHeight() * 0.7f;
+        topLeft_ = glm::vec2((ww- size_)*.5, (wh - size_) *.5);
 
         valueGrid_ = make_shared<ofxValueGrid<ofxValueGridCell>>(55,55,size_,size_, glm::vec2(.5f,.5f));
 
         fbo_.reset();
         fbo_ = make_unique<ofFbo>();
-        fbo_->allocate(size_, size_, GL_RGBA);
+        fbo_->allocate(ww, wh, GL_RGBA);
 
         drawPerlinFlow();
     }
@@ -95,12 +103,15 @@ private:
     void drawPerlinFlow()
     {
         fbo_->begin();
+            ofPushMatrix();
+
             ofSetCircleResolution(100);
             ofClear(background_);
             ofSetRectMode(OF_RECTMODE_CENTER);
 
             ofSetLineWidth(2);
 
+            ofTranslate(topLeft_);
             for (const auto& c : valueGrid_->cells())
             {
                 auto noiseCol00 = ofNoise(c->pointX() * 0.002, c->pointY() * 0.002, animation_ * 0.2) / 1.0;
@@ -122,6 +133,7 @@ private:
 
                     ofPopMatrix();
             }
+            ofPopMatrix();
         fbo_->end();
     }
 };
