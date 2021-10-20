9f5b6848440550f74d1a6ce3a2241a75199a3b88
Constant Dupuis <constant.dupuis@gmail.com>
2021-10-20 21:39:42 +0200

    Make it drawing

--- ofxSubSketch/src/DrawingTest.h
+++ ofxSubSketch/src/DrawingTest.h
@@ -13,7 +13,6 @@ public:
         ofSetBackgroundAuto(false);
         ofBackground( background_ );
         ofFill();
-
     }
 
     void update() {
@@ -21,36 +20,47 @@ public:
     }
 
     void draw() {
-
+        if (ofGetMousePressed(OF_MOUSE_BUTTON_LEFT)) {
+            ofSetColor(draw_color_);
+            ofDrawRectangle(ofGetMouseX(), ofGetMouseY(), 30,30);
+        }
 
         //drawUI();
     }
 
     void mousePressed(int x, int y, int button) {
-        ofSetColor(draw_color_);
-        ofDrawRectangle(x, y, 10,10);
+
     }
+
+    void mouseMoved(int x, int y)
+    {
+
+    }
+
+    void mouseReleased( int x, int y)
+    {}
 
 private:
 
     ofxImGui::Gui gui;
     ofColor background_ = ofColor(255);
-    ofColor draw_color_ = ofColor(0);
+    ofColor draw_color_ = ofColor(0,40,60,10);
 
     ImVec4 imColor_;
 
+
     void drawUI()
     {
-        gui.begin();
-            imColor_.x = (float)background_.r/255.0;
-            imColor_.y = (float)background_.g/255.0;
-            imColor_.z = (float)background_.b/255.0;
-            imColor_.w = (float)background_.a/255.0;
-            ImGui::ColorEdit4("background", (float*)&imColor_);
-            background_.r = imColor_.x*255;
-            background_.g = imColor_.y*255;
-            background_.b = imColor_.z*255;
-            background_.a = imColor_.w*255;
-        gui.end();
+//        gui.begin();
+//            imColor_.x = (float)background_.r/255.0;
+//            imColor_.y = (float)background_.g/255.0;
+//            imColor_.z = (float)background_.b/255.0;
+//            imColor_.w = (float)background_.a/255.0;
+//            ImGui::ColorEdit4("background", (float*)&imColor_);
+//            background_.r = imColor_.x*255;
+//            background_.g = imColor_.y*255;
+//            background_.b = imColor_.z*255;
+//            background_.a = imColor_.w*255;
+//        gui.end();
     }
 };
