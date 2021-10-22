98676b90e5f70bbf6e905b06f03e253b3dc97f0a
Constant Dupuis <cdupuis@apiquiet.com>
2021-10-22 11:47:56 +0200

    refactoring, j'aime ça

--- ofxSubSketch/src/painters/NoiseFlowWormPainter.h
+++ ofxSubSketch/src/painters/NoiseFlowWormPainter.h
@@ -12,42 +12,85 @@ class Easel {
 public:
     Easel(){}
     Easel( int view_width, int view_height, int canvas_width, int canvas_height) {
-        view_width_ = view_width;
-        view_height_ = view_height;
-        canvas_width_ = canvas_width;
-        canvas_height_ = canvas_height;
+        view_size_.x = view_width;
+        view_size_.y = view_height;
+        canvas_size_.x = canvas_width;
+        canvas_size_.y = canvas_height;
+        allocateFbo();
         calculate();
     }
 
-    int getViewWidth() { return view_width_;}
-    int getViewHeight() { return view_height_;}
-    int getCanvasWidth() { return canvas_width_;}
-    int getCanvasHeight() { return canvas_height_;}
+    Easel( glm::vec2 view_size, glm::vec2 canvas_size) {
+        view_size_ = view_size;
+        canvas_size_ = canvas_size;
+        allocateFbo();
+        calculate();
+    }
+
+
+    int getViewWidth() { return view_size_.x;}
+    int getViewHeight() { return view_size_.y;}
+    int getCanvasWidth() { return canvas_size_.x;}
+    int getCanvasHeight() { return canvas_size_.y;}
+
+    const glm::vec2& getViewSize() { return view_size_;}
+    const glm::vec2& getCanvasSize() { return canvas_size_;}
+    const glm::vec2& getCanvasPos() { return canvas_pos_;}
+
+    void setViewSize(glm::vec2 view_size) {
+        view_size_ = view_size;
+        calculate();
+    }
 
     void setViewSize(int view_width, int view_height) {
-        view_width_ = view_width;
-        view_height_ = view_height;
+        view_size_.x = view_width;
+        view_size_.x = view_height;
+        calculate();
+    }
+
+    void setCanvasSize(glm::vec2 canvas_size) {
+        canvas_size_ = canvas_size;
+        allocateFbo();
         calculate();
     }
 
     void setCanvasSize(int canvas_width, int canvas_height) {
-        canvas_width_ = canvas_width;
-        canvas_height_ = canvas_height;
+        canvas_size_.x = canvas_width;
+        canvas_size_.y = canvas_height;
+        allocateFbo();
         calculate();
     }
 
-    glm::vec2 getCanvasPos() { return canvas_pos_;}
+    void begin() { fbo_.begin(); }
+    void end() { fbo_.end(); }
+    void drawCanvas() {
+        ofPushMatrix();
+            ofTranslate(canvas_pos_);
+            fbo_.draw(0,0);
+        ofPopMatrix();
+    }
+    void drawCanvas(int x, int y) {fbo_.draw(x,y);}
+
 private:
     glm::vec2 canvas_pos_;
-    int view_width_ = 1000;
-    int view_height_ = 1000;
-    int canvas_width_ = 400;
-    int canvas_height_ = 400;
+    glm::vec2 view_size_ {1000,1000};
+    glm::vec2 canvas_size_ {400,400};
+    ofFbo fbo_;
 
     void calculate()
     {
-        canvas_pos_.x = (view_width_- canvas_width_) / 2;
-        canvas_pos_.y = (view_height_- canvas_height_) / 2;
+        canvas_pos_.x = (view_size_.x - canvas_size_.x) / 2;
+        canvas_pos_.y = (view_size_.y - canvas_size_.y) / 2;
+    }
+
+    void allocateFbo()
+    {
+        if( fbo_.isAllocated())
+        {
+            fbo_.clear();
+        }
+
+        fbo_.allocate(canvas_size_.x, canvas_size_.y, GL_RGBA);
     }
 };
 
@@ -60,7 +103,8 @@ public:
         ofSetVerticalSync(true);
         ofSetBackgroundAuto(false);
 
-        localSetup();
+        easel_.setViewSize(ofGetWidth(), ofGetHeight());
+        easel_.setCanvasSize(ofGetHeight() * 0.75, ofGetHeight() * 0.75);
     }
 
     void update()
@@ -70,12 +114,13 @@ public:
 
     void draw()
     {
-        ofPushMatrix();
-        ofTranslate(easel_.getCanvasPos());
+        // draw on the canvas (ofFbo)
+        easel_.begin();
+            // draw on canvas here
+        easel_.end();
 
-
-
-        ofPopMatrix();
+        // draw the canvas (ofFbo)
+        easel_.drawCanvas();
     }
 
     void windowResized(int w, int h) {
